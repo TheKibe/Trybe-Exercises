098cc54bd29e17b21375735a60417ca2e67b3102
Juan <juammgsvo@hotmail.com>
2022-04-19 17:03:10 -0300

    calendario trybe exercicio 1,2,3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/calendario-tryber/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/calendario-tryber/script.js
@@ -14,3 +14,58 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+    
+  //Exercício 1:
+
+  function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const idDays = document.querySelector('#days')
+
+    for (i = 0; i < dezDaysList.length; i += 1) {
+        const dezDays = dezDaysList[i];
+        const liDays = document.createElement('li');
+        liDays.innerHTML = dezDays;
+        liDays.className = 'day';
+
+        if (dezDays === 24 || dezDays === 25 || dezDays === 31) {
+            liDays.classList.add('holiday');
+        };
+        if (dezDays === 4 || dezDays === 11 || dezDays === 18 || dezDays === 25) {
+            liDays.classList.add('friday');
+        };
+
+        idDays.appendChild(liDays);
+    };
+  };
+
+  createDaysOfTheMonth();
+
+  //Exercício 2:
+
+  function createHolidays(feriados) {
+      const button = document.createElement('button');
+      button.id = 'btn-holiday';
+      button.innerHTML = feriados;
+      document.querySelector('.buttons-container').appendChild(button);    
+  };
+
+  createHolidays('Feriados');
+
+  //Exercício 3:     Falta lógica inversa.
+
+  const holidayButton = document.querySelector('#btn-holiday');
+  const holidayDays = document.querySelectorAll('.holiday');
+  
+  holidayButton.addEventListener('click', function() {
+    for (i = 0; i < holidayDays.length; i += 1) {
+        const count = holidayDays[i];
+        count.style.backgroundColor = 'rgb(242, 234, 119)';
+        if (count.attributes === true) {
+            count.removeAttribute('style');
+        }
+    }
+  });
+
+  //Exercício 4:
+
+  
