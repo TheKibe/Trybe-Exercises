e7d90060435e8fe87d222519af44111f5cdc50cf
juan <juammgsvo@hotmail.com>
2022-07-20 20:15:51 -0300

    pokedex finished

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/components/Pokedex.jsx
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/components/Pokedex.jsx
@@ -4,7 +4,14 @@ import pokemons from '../data.js'
 
 class Pokedex extends Component {
     render() {
-        const pokemonsList = pokemons.map(index => <ul key={index.id} className="pokemon"><Pokemon pokemon={index} /></ul>)
+        const pokemonsList = pokemons.map(index => (
+            <Pokemon
+                key={index.id}
+                pokemonName={index.name}
+                pokemonImage={index.image}
+                pokemonType={index.type}
+                pokemonWeight={index.averageWeight} />
+        ));
         return (
             <div>
                 <div><h1 className="title">Pokédex</h1></div>
--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/components/Pokemon.jsx
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/components/Pokemon.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 class Pokemon extends Component {
     render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
+        const { pokemonImage } = this.props;
+        const { pokemonName } = this.props;
+        const { pokemonType } = this.props;
+        const { pokemonWeight } = this.props;
         return (
-            <div>
-                <img src={image} alt="PokemonPhoto" />
-                <p>{name}</p>
-                <p>{type}</p>
-                <p>averageWeight: {averageWeight.value} {averageWeight.measurementUnit}</p>
-            </div>
+            <ul className="pokemon">
+                <img src={pokemonImage} alt="PokemonPhoto" />
+                <p>{pokemonName}</p>
+                <p>{pokemonType}</p>
+                <p>averageWeight: {pokemonWeight.value} {pokemonWeight.measurementUnit}</p>
+            </ul>
         )
     }
 }
 
+Pokemon.propTypes = {
+    pokemonImage: PropTypes.string,
+    pokemonName: PropTypes.string,
+    pokemonType: PropTypes.string,
+    pokemonWeight: PropTypes.object,
+}
+
+Pokemon.defaultProps = {
+    pokemonImage: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    pokemonName: 'Pikachu',
+    pokemonType: 'Electric',
+    pokemonWeight: {
+        value: 6.0,
+        measurementUnit: "kg"
+    },
+}
+
 export default Pokemon;
+
